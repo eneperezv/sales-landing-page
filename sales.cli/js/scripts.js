cbe537358ae8a35bfd9157a12c03db32ed1c30e4
eneperezv <eneperezv@gmail.com>
2024-10-11 22:26:56 -0500

    Se modifica index para incluir jumbotron y efecto paralax
     Changes to be committed:
    	new file:   sales.cli/assets/images/pexels-negativespace-34577.jpg
    	modified:   sales.cli/css/styles.css
    	modified:   sales.cli/index.html
    	modified:   sales.cli/js/scripts.js

--- sales.cli/js/scripts.js
+++ sales.cli/js/scripts.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Página cargada y lista para aumentar tus ventas 🚀');
 });
+
+document.addEventListener('scroll', function() {
+    const parallax = document.querySelector('.parallax');
+    let scrollPosition = window.scrollY;
+
+    // Adjust the background position based on scroll
+    parallax.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
+});
